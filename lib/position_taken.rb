c8f2fe89a37645b8a23b54f8fc01f415127844a8
Matt Farmer <mrfarmer777@gmail.com>
2018-03-28 04:20:35 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken(board,ind)
-  if (board[ind]==" " || board[ind]=="" || board[ind]==nil)
-    false
-  else
+  if (board[ind]=="X" || board[ind]=="O")
     true
+  else
+    false
   end
 end
